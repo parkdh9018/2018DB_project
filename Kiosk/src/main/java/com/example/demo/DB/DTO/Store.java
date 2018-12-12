10f492e502ad3fa9480a993e036bf9542a6fe5c4
parkdh9018 <parkdh9018@naver.com>
2018-12-12 21:30:20 +0900

    Store.java 추가

---
+++ Kiosk/src/main/java/com/example/demo/DB/DTO/Store.java
@@ -0,0 +1,42 @@
+package com.example.demo.DB.DTO;
+
+public class Store {
+  private String address;
+  private String phone;
+  private String manager;
+  private String crn;
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+
+  public String getManager() {
+    return manager;
+  }
+
+  public void setManager(String manager) {
+    this.manager = manager;
+  }
+
+  public String getCrn() {
+    return crn;
+  }
+
+  public void setCrn(String crn) {
+    this.crn = crn;
+  }
+}
